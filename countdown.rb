e53cd45c1a128180f93e03d841df761773c7d2cf
Naomi Katz <naykatz@gmail.com>
2019-02-15 21:33:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(number)
   while number > 0 
-    puts "#{number} SECONDS!"
+    puts "#{number} SECOND(S)!"
     number -= 1 
   end
   "HAPPY NEW YEAR!"
@@ -10,7 +10,7 @@ end
 
 def countdown_with_sleep(number)
   while number > 0 
-    puts "#{number} SECONDS!"
+    puts "#{number} SECOND(S)!"
     sleep(1)
     number -= 1 
   end
