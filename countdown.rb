7f5a7f10713ec9d55a661a91498b894648862782
Naomi Katz <naykatz@gmail.com>
2019-02-15 21:29:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0 
+    puts "#{number} SECONDS"
+    number -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number > 0 
+    puts "#{number} SECONDS"
+    sleep(1)
+    number -= 1 
+  end
+  "HAPPY NEW YEAR!"
 end
